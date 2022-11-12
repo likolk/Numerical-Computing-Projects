24955e740165a6dca2b404ae8df1e5585c6b12c3
Kelvin Likollari <likolk@usi.ch>
2022-11-12 12:31:00 +0100

    Update cluster_points.jl

--- Project 4/Project4_students/cluster_points.jl
+++ Project 4/Project4_students/cluster_points.jl
@@ -30,6 +30,7 @@ dummy_ϵ = 1;
 # S = similarity(pts_dummy[:, 1:2]); changed to :
 S = similarity(pts_spiral[:, 1:2]);
 
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------
 # 1b) Find the mininal spanning tree of the full graph
 minimal_spanning_tree = minspantree(S);
 #   Compute epsilon
@@ -37,17 +38,20 @@ minimal_spanning_tree = minspantree(S);
 
 dummy_ϵ = ϵ;
 
-
+# --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # 1c) Compute the epsilon similarity graph
 # G_e = epsilongraph(dummy_ϵ, pts_dummy); changed to
 G_e = epsilongraph(dummy_ϵ, pts_spiral);
 
+# --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 # 1d) Create the adjacency matrix for the epsilon case
 W_e = S .* G_e;
 # draw_graph(W_e, pts_dummy) changed to
 draw_graph(W_e, pts_spiral)
 
+
+# --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # 1e) Create the Laplacian matrix and implement spectral clustering.
 L, D = createlaplacian(W_e);
 
@@ -64,20 +68,54 @@ eigenvectors = eigenvectors[:, sortperm(eigenvalues)];
 # get the first K = 2 eigenvectors
 eigenvectors = eigenvectors[:, 1:K]; # get the first K = 2 columns of the matrix
 
-# use function kmeans() to cluster 
-# the rows of these eigenvectors.
 
+#   K-means method to cluster rows of these eigenvectors 
+clustered_eigenvalues, clustered_eigenvectors = kmeans(pts_spiral, K, n)
+
+# --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 # 1f) Run K-means on input data
 # R = kmeans(pts_dummy', K); changed to:
-R = kmeans(pts_spiral', K);
-data_assign = R.assignments;
+# data_assign = R.assignments; # TODO: What does this line do? 
+eigenvalues_clustering, eigenvectors_clustering = kmeans(pts_spiral, K, n) 
+# spec_assign = R.assignments; # TODO: What does this line do too?
 
 
-#   Cluster rows of eigenvector matrix of L corresponding to K smallest eigenvalues. Use kmeans as above.
-R = kmeans(pts_dummy', K);
-spec_assign = R.assignments;
+
+# ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 # 1h) Visualize spectral and k-means clustering results
-draw_graph(W_e, pts_dummy, data_assign)
-draw_graph(W_e, pts_dummy, spec_assign)
+# draw_graph(W_e, pts_dummy, data_assign)
+# draw_graph(W_e, pts_dummy, spec_assign)
+
+draw_graph(W_e, pts_spiral, clustered_eigenvectors)
+draw_graph(W_e, pts_spiral, eigenvectors_clustering)
+
+
+# TODO: 1.7
+
+# Cluster the datasets i) Two spirals, ii) Cluster in cluster, and iii) Crescent & full moon 
+# in K = 2 clusters, and visualize the results obtained with spectral clustering and 
+# k-means directly on the input data. Do the same for i) Corners, and ii) Outlier 
+# for K = 4 clusters.
+
+
+# for two_spirals:
+two_spiral, nothing, nothing, nothing, nothing, nothing = getpoints();
+two_spiral_map = rand(1:K, size(two_spiral, 1));
+two_spiral_ϵ = 1;
+two_spiral_S = similarity(two_spiral[:, 1:2]);
+two_spiral_minimal_spanning_tree = minspantree(two_spiral_S);
+two_spiral_ϵ = maximum(two_spiral_minimal_spanning_tree);
+two_spiral_G_e = epsilongraph(two_spiral_ϵ, two_spiral);
+two_spiral_W_e = two_spiral_S .* two_spiral_G_e;
+two_spiral_L, two_spiral_D = createlaplacian(two_spiral_W_e);
+two_spiral_eigenvalues, two_spiral_eigenvectors = eigs(two_spiral_L, nev = K, which = :SR);
+two_spiral_eigenvalues = sort(two_spiral_eigenvalues);
+two_spiral_eigenvectors = two_spiral_eigenvectors[:, sortperm(two_spiral_eigenvalues)];
+two_spiral_eigenvectors = two_spiral_eigenvectors[:, 1:K];
+two_spiral_clustered_eigenvalues, two_spiral_clustered_eigenvectors = kmeans(two_spiral, K, n)
+draw_graph(two_spiral_W_e, two_spiral, two_spiral_clustered_eigenvectors)
+draw_graph(two_spiral_W_e, two_spiral, two_spiral_eigenvectors_clustering)
+
+
